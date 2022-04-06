fa4a53d6e49c69e4986d83b7a0c41342bd75fdb2
ado durak <76266634+Alginest@users.noreply.github.com>
2022-04-06 20:16:47 +0200

    Add files via upload

---
+++ src/components/AllHome/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles((theme) => ({
+  allHome: {
+    width: "100%",
+    height: "3000px",
+    backgroundColor: "rgb(20,20,20)",
+  },
+}));
